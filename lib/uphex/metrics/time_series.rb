1e46a5fe9de1eeaa997f6d8b0570e6d91af7a63a
John Feminella <jxf@jxf.me>
2014-10-01 02:28:06 -0400

    Implement Enumerable on TimeSeries

--- lib/uphex/metrics/time_series.rb
+++ lib/uphex/metrics/time_series.rb
@@ -3,12 +3,18 @@ require 'uphex/metrics/time_series_entry'
 
 module UpHex::Metrics
   class TimeSeries
+    include Enumerable
+
     attr_reader :series
 
     def initialize(array_of_time_series_entries)
       @series = array_of_time_series_entries.
         map { |e| TimeSeriesEntry.to_time_series_entry e }.
         sort_by(&:date)
+    end
+
+    def each(*args, &block)
+      @series.each(*args, &block)
     end
   end
 end
