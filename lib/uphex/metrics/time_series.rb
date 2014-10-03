76cb004a05d7040f6dd1efc3e96f859f735ff8fb
John Feminella <jxf@jxf.me>
2014-10-03 04:20:52 -0400

    Implement Forwardable on TimeSeries and delegate to @series

--- lib/uphex/metrics/time_series.rb
+++ lib/uphex/metrics/time_series.rb
@@ -1,12 +1,18 @@
 require 'time'
+require 'forwardable'
 require 'uphex/metrics'
 require 'uphex/metrics/time_series_entry'
 
 module UpHex::Metrics
   class TimeSeries
     include Enumerable
+    extend Forwardable
 
     attr_reader :series
+
+    def_delegators :@series,
+      :each,
+      :size
 
     def initialize(array_of_time_series_entries)
       @series = array_of_time_series_entries.
@@ -15,8 +21,5 @@ module UpHex::Metrics
         uniq(&:date)
     end
 
-    def each(*args, &block)
-      @series.each(*args, &block)
-    end
   end
 end
