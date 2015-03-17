79764147f59480d1adf33b90e5a478d22ce86b72
Thomaz Leite <i@thomazleite.com>
2015-03-17 16:00:17 -0700

    Adjust ARIMA script

--- arima_run.rb
+++ arima_run.rb
@@ -2,11 +2,10 @@ $: << "lib"
 
 require "uphex/metrics/autoregressive_integrated_moving_average"
 require "uphex/metrics/time_series"
-require "uphex/metrics"
 
 now = Time.now.utc
-val = ARGV[0].to_f || 73
-len = ARGV[1].to_i || 100
+val = Float(ARGV[0] || 73.0)
+len = Integer(ARGV[1] || 100)
 t_s = UpHex::Metrics::TimeSeries.new(len.times.map { |i| [now.advance(days: i - len), val] })
 
 puts UpHex::Metrics::AutoregressiveIntegratedMovingAverage.new(t_s).forecast
